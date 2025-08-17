86f404c0706a4cc78a3bb29da3937be4dd92ba6b
JayanthiLakshmanan <jaya15493@gmail.com>
2025-08-17 01:14:40 -0700

    Add files via upload
    week 1day 1 ass 1

---
+++ ass2/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package week1.day1.ass2;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="John Doe";
+		int age=30;
+		double salary=55000.75;
+		char gender='M';
+		boolean acceptTerms=true;
+		float rating=4.5f;
+		long mobileNumber=9876543210L;
+		int pinCode=56001;
+		int luckyNumber=7;
+		System.out.println("Name:"+name);
+		System.out.println("Age:"+age);
+		System.out.println("Salary:"+salary);
+		System.out.println("Gender:"+gender);
+		System.out.println("AcceptTerms:"+acceptTerms);
+		System.out.println("Rating:"+rating);
+		System.out.println("MobileNumber:"+mobileNumber);
+		System.out.println("PinCode:"+pinCode);
+		System.out.println("LuckyNumber:"+luckyNumber);
+
+	}
+
+}
